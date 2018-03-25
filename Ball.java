7fe2ab335b1b0de980e35b20fca0e907676a8a9c
Alan Meile <alan.meile@gmail.com>
2018-03-25 11:25:41 +0200

    fixed displaying a JComponent as discussed

--- Ball.java
+++ Ball.java
@@ -1,67 +1,25 @@
 package ch.hslu.ad.sw05;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-
+import java.awt.Insets;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Ball implements Runnable
-{
-
-	private Color ballFillColor;
-    private Color ballBorderColor;
-     
-    private int ballX = 0;
-    private int ballY = 0;
-    private int ballWidth = 0;
-    private int ballHeight = 0;
-     
-    public boolean fillBall = true;
+public class Ball extends JComponent {
+    private static final long serialVersionUID = 1L;
     
-    JPanel jp = null;
-    
-    Ball(int ballX, int ballY, int ballWidth, int ballHeight, Color ballBorderColor, JPanel jp){ //Constructor
-        this.ballFillColor=ballBorderColor;
-        this.ballBorderColor=ballBorderColor;
-        this.ballWidth=ballWidth;
-        this.ballHeight=ballHeight;
-        this.ballX=ballX;
-        this.ballY=ballY;
-        this.jp=jp;
-    }
-	
-    //public void drawBall(JPanel jp) 
-    //{
-    //    jp.add(new MyComponent());
-    //}
-    
-	private class MyComponent extends JComponent{
-		@Override
-        public void paintComponent(Graphics g){
-			super.paintComponent(g);
-			//g.setPaintMode();
-            System.out.println(ballFillColor+"test");
-            if (fillBall) //Fill first, and then draw outline.
-            {
-                g.setColor(ballFillColor);
-                g.fillOval(ballX,ballY, ballHeight,ballWidth);
-            }
-            
-            g.setColor(ballFillColor);
-            g.drawOval(ballX,ballY, ballHeight,ballWidth);
-            System.out.println("?");
-            
-            jp.revalidate();
-        }
+    public Ball(int x, int y, int diameter) {
+        super();
+        this.setLocation(x, y);
+        this.setSize(diameter, diameter);
     }
 
-	@Override
-	public void run() 
-	{
-		jp.add(new MyComponent());
-		//jp.revalidate();
-		//jp.repaint();
-	}
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.setColor(Color.red);
+        g.fillOval(0, 0, 100, 100);
+    }
 }
--- NR4.java
+++ NR4.java
@@ -1,6 +1,6 @@
 package ch.hslu.ad.sw05;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 public class NR4 {
 
 	public static void main(String[] args) {
-		KreiseView frame=new KreiseView();
+            KreiseView frame=new KreiseView();
 	    frame.setTitle("Word Cloud");
 	    frame.setSize(1000, 620);
 	    frame.setResizable(false);
@@ -17,37 +17,25 @@ public class NR4 {
 	    frame.setVisible(true);
 	    frame.setLayout(new java.awt.FlowLayout());
 	    frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-	    
+            
 	    JPanel jp = new JPanel();
 	    jp.setBackground(new java.awt.Color(200, 200, 200));
-	    //jp.setSize(1000, 620);
+            jp.setLayout(new BorderLayout());
 	    frame.setContentPane(jp);
-	    
+            
 	    frame.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-            	System.out.println(e.getX()+" "+e.getY());
-            	
-            	//Ball redBall = new Ball(e.getX(),e.getY(),100,100,Color.red, jp);
-            	frame.setContentPane(jp);
-                
-                Thread redBall = new Thread(new Ball(e.getX(),e.getY(),100,100,Color.red, jp),"Ball1");
-                redBall.start();
-                //frame.setContentPane(jp);
-                
-                //frame.validate();
-                //frame.repaint();
-                //jp.revalidate();
-                //jp.repaint();
-                
-            }
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    Ball ball = new Ball(e.getX(), e.getY(), 5);
+                    jp.add(ball);
+                    
+                    jp.revalidate();
+                }
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                //System.out.println("Mouse released");
-            }
-        });
-
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    
+                }
+            });
 	}
-
 }
